a855bda0353f29c9644f48afd24d2addc3959077
Arthur Novaes <arthurnovaes9@gmail.com>
2016-11-28 17:49:25 -0300

    /todos delete coments

--- server.js
+++ server.js
@@ -35,23 +35,7 @@ app.get('/todos', function (req, res) {
 		res.json(todos);
 	}, function (e) {
 		res.status(500).send();
-	})
-
-	// var filteredTodos = todos;
-
-	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	// 	filteredTodos = _.filter(filteredTodos, {completed: true});
-	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	// 	filteredTodos = _.filter(filteredTodos, {completed: false});
-	// }
-
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function (todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; 
-	// 	});
-	// }
-	
-	// res.json(filteredTodos);
+	});
 });
 
 // GET /todos/:id
